ca18d9fedd1ac00430fb76535f66c8785762b8fa
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-16 18:19:20 -0700

    add angular with routing

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EditComponent } from './edit/edit.component';
+import { IndexComponent } from './index/index.component';
+import { NewAuthorFormComponent } from './new-author-form/new-author-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: IndexComponent},
+  { path: '/new', component: NewAuthorFormComponent },
+  { path: '/edit', component: EditComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
